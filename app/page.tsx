eafd825ea8f7dd51f5d02414526da9589c6176bd
0831leesr <0831leesr@naver.com>
2026-05-06 14:46:28 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import RichzemHome from "@/components/richzem-home"
 
-/** 60초마다 재검증 - getTopLiveGames ISR revalidate(60s)와 맞춤 */
-export const revalidate = 60
+/** 60초마다 재검증 - getTopLiveGames ISR revalidate(60s)와 맞춤
+export const revalidate = 60 */
+
+//페이지 레벨 캐싱 해제
+export const dynamic = 'force-dynamic'
 
 export default function Page() {
   return <RichzemHome />
